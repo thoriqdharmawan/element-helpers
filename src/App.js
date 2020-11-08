4b29a9439bb01126bb8341aa57c8eed973870dd7
thoriqdharmawan <thoriqd21@gmail.com>
2020-11-08 21:33:44 +0700

    draggable element

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import { useGetDimensions } from "element-helpers";
+import {useDraggableElement} from './Hooks'
+import './index.css'
 
 function App() {
-  const [refNode, { height, width }] = useGetDimensions();
+  // const [refNode, { height, width }] = useGetDimensions();
 
-  console.log("height : ", height); // 100
-  console.log("width : ", width); // 300
+  const [refNode] = useDraggableElement()
 
   return (
-    <div
-      ref={refNode}
-      style={{ width: 300, height: 100, backgroundColor: "red" }}
-    />
+    <div style={{
+      width: 400,
+      height: 400,
+      border: '1px solid black',
+    }} >  
+  
+      <div 
+        ref={refNode} 
+        className="dumy xs"
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'red',
+          position: "absolute",
+          cursor: "grab",
+        }}
+      />
+      
+      <div className="dumy xm"></div>
+      <div className="dumy md"></div>
+      <div className="dumy lg"></div>
+    </div>
   );
 }
 
