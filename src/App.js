0e9fd29cb5d069c485861529ec0120b0ec7aebaa
thoriqdharmawan <thoriqd21@gmail.com>
2020-12-16 23:13:12 +0700

    fix draggable

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import {useDraggableElement} from './Hooks'
 import './index.css'
-import logo from './asset/logo512.png';
 
 function App() {
-  // const [refNode, { height, width }] = useGetDimensions();
-
-  const [refNode] = useDraggableElement()
-  const [refNode2] = useDraggableElement()
+  const refNode = useDraggableElement()
 
   return (
-    <div>
-      <img 
-        ref={refNode2}  
-        src={logo} 
-        style={{
-          position: "absolute",
-          cursor: "grab",
-        }} 
-      />
-      <div 
-        ref={refNode} 
-        className="dumy xs"
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: 'red',
-          position: "absolute",
-          cursor: "grab",
-        }}
-      />
-    </div>
+    <div ref={refNode} className="box" />
   );
 }
 
